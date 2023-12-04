bee586068a4562031714130e1a29e35b5478dcdb
Mateusz Wodecki <matwodecki@gmail.com>
2023-12-04 21:35:26 +0100

    Status - zmiana nazw przypadków i dodanie sprawdzania wartości w bazie danych

--- api/src/Enum/Status.php
+++ api/src/Enum/Status.php
@@ -4,9 +4,9 @@ namespace App\Enum;
 
 enum Status: string
 {
-    case nowe = "nowe";
-    case wNaprawie = 'w naprawie';
-    case czekaNaOdbior = 'czeka na odbiór';
-    case odebranePrzezKlienta = 'odebrane przez klienta';
-    case anulowane = 'anulowane';
+    case new = "nowe";
+    case repairing = 'w naprawie';
+    case waiting = 'czeka na odbiór';
+    case pickedUp = 'odebrane przez klienta';
+    case cancelled = 'anulowane';
 }
